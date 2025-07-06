4a2631c5d198fa7ea6738630e31ef595c96e5d7a
vzateychuk <vzateychuk@gmail.com>
2025-07-06 17:15:17 +0300

    feat: implement health check endpoint and refactor main application structure

---
+++ tests/health_check.rs
@@ -0,0 +1,41 @@
+//! tests/health_check.rs
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the`#[test]`attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand--testhealth_check` (<-name of the testfile)
+
+#[tokio::test]
+async fn health_check_works() {
+    //Arrange
+    spawn_app(); // No .await, no .expect
+
+    //We need to bring in`reqwest`
+    //to perform HTTP requests agains tour application.
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background ~somehow~
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to spawn our app.");
+
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+}
